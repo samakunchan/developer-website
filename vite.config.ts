28015ec8ed9fcda253ad5fa42051e9eeeb074c43
samakunchan <cedric.badjah@gmail.com>
2026-04-05 22:49:50 +0200

    fix: warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { nitro } from 'nitro/vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react-swc';
 import { lingui } from '@lingui/vite-plugin';
@@ -10,8 +9,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tsConfigPaths(),
     tanstackStart(),
     nitro(),
     // react's vite plugin must come after start's vite plugin
