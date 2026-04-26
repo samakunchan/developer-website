374b2cd634defa92439981e3c4fc735f6d7d8201
samakunchan <cedric.badjah@gmail.com>
2026-04-26 05:12:04 +0200

    feat: shells
    - Updated shells commands.

--- shells/docker-build-dev.sh
+++ shells/docker-build-dev.sh
@@ -1,3 +1,3 @@
-docker compose down -v &&
-docker compose -f compose.yml -f compose-dev.yml --env-file docker.env up --build -d &&
+docker compose -p developer-website-dev down -v &&
+docker compose -p developer-website-dev -f compose.yml -f compose-dev.yml --env-file docker.env up --build -d &&
 echo "Build dev complete...."
--- shells/docker-build-prod.sh
+++ shells/docker-build-prod.sh
@@ -1,3 +1,3 @@
-docker compose down -v &&
-docker compose -f compose.yml -f compose-prod.yml --env-file docker-prod.env up --build -d &&
+docker compose -p developer-website-prod down -v &&
+docker compose -p developer-website-prod -f compose.yml -f compose-prod.yml --env-file docker-prod.env up --build -d &&
 echo "Build prod complete...."
